8463c148fd279fe11a6365af8a567704a7c7f5d9
Steve Suh <stevesuh54@gmail.com>
2016-01-11 14:24:43 -0500

    Add exception handling to deal with Twitter and/or Wikipedia API problems

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,14 +4,24 @@ class SearchController < ApplicationController
   end
 
   def search
-    @tweets = TWITTER_CLIENT.search(params[:q]).take(TWITTER_SEARCH_LIMIT).collect { |t| TwitterResult.new(t)}
-    @wikis = []
-    WIKI_CLIENT.custom_query(list: "search", srsearch: params[:q], srwhat: "text",
-                                      srlimit: WIKI_SEARCH_LIMIT)[1].each_element do |w|
-      @wikis << WikipediaResult.new(w.attributes['title'], w.attributes['snippet'],
-                          w.attributes['size'], w.attributes['wordcount'],
-                          w.attributes['timestamp'])
+    begin
+      @tweets = TWITTER_CLIENT.search(params[:q]).take(TWITTER_SEARCH_LIMIT).collect { |t| TwitterResult.new(t)}
+    rescue Twitter::Error::ServiceUnavailable => e
+      @tweets = []
     end
+
+    begin
+      @wikis = []
+      WIKI_CLIENT.custom_query(list: "search", srsearch: params[:q], srwhat: "text",
+                                        srlimit: WIKI_SEARCH_LIMIT)[1].each_element do |w|
+        @wikis << WikipediaResult.new(w.attributes['title'], w.attributes['snippet'],
+                            w.attributes['size'], w.attributes['wordcount'],
+                            w.attributes['timestamp'])
+      end
+    rescue MediaWiki::Exception=>e
+      @wikis = []
+    end
+
     return @tweets, @wikis
   end
 end
