d755ad85bb817a99c739c6d08e143ec26735b750
Steve Suh <stevesuh54@gmail.com>
2016-01-11 13:52:49 -0500

    Make the twitter result an HTML link as well

--- app/models/twitter_result.rb
+++ app/models/twitter_result.rb
@@ -1,9 +1,10 @@
 class TwitterResult
   include ActiveModel::Model
-  attr_accessor :user_screen_name, :text
+  attr_accessor :user_screen_name, :text, :url
 
   def initialize(tweet)
     @user_screen_name = tweet.user.screen_name
     @text = tweet.text
+    @url = tweet.url
   end
 end
